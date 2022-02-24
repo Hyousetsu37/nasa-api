3f76a240f5e7e09914be8c856d890e9d25016195
xHyousetsu <cristiandml37@utp.edu.co>
2022-02-24 18:40:17 -0500

    moved the afterAll to the end

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -9,10 +9,7 @@ describe('Launches API', ()=>{
         await mongoConnect();
     });
 
-    afterAll(async ()=>{
-        await mongoDisconnect();
-    });
-
+    
     describe('Test GET /launches', ()=>{
         test('It should respond with 200 success', async ()=>{
             const response = await request(app)
@@ -41,43 +38,46 @@ describe('Launches API', ()=>{
             target: 'Kepler-1652 b',
             launchDate: 'InvalidDate',
         };
-    
+        
         test('It should respond with 201 created', async ()=>{
             const response = await request(app)
             .post('/v1/launches')
             .send(completeLaunchData)
             .expect('Content-Type', /json/)
             .expect(201);
-    
+            
             const requestDate = new Date(completeLaunchData.launchDate).valueOf();
             const responseDate = new Date(response.body.launchDate).valueOf();
             expect(responseDate).toBe(requestDate);
-    
+            
             expect(response.body).toMatchObject(launchDataWithoutDate);
         });
-    
+        
         test('It should catch missing required properties', async ()=>{
             const response = await request(app)
             .post('/v1/launches')
             .send(launchDataWithoutDate)
             .expect('Content-Type', /json/)
             .expect(400);
-    
+            
             expect(response.body).toStrictEqual({
                 error: 'missing required launch property',
             });
         });
-    
+        
         test('It should catch invalid dates', async ()=>{
             const response = await request(app)
             .post('/v1/launches')
             .send(launchDataWithAnInvalidDate)
             .expect('Content-Type', /json/)
             .expect(400);
-    
+            
             expect(response.body).toStrictEqual({
                 error: 'Invalid launch date',
             });
+        });
+        afterAll(async ()=>{
+            await mongoDisconnect();
         });
     });
 })
